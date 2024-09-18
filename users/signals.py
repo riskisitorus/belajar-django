ae3c66896c74998b7a47c8e536211ccccbca0353
riskisitorus <riskijkm@gmail.com>
2024-09-18 18:41:44 +0700

    send welcome email to new users

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
-from .models import Profile, User
+from django.contrib.auth.models import User
+from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 
 # @receiver(post_save, sender=Profile)
 def CreateProfile(sender, instance, created, **kwargs):
@@ -13,6 +15,16 @@ def CreateProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
         )
+
+        send_mail(
+            "Welcome!",
+            "Welcome to this App :D",
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
+        print(send_mail)
 
 @receiver(post_save, sender=Profile)
 def UpdateProfile(sender, instance, created, **kwargs):
