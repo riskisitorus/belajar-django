cbbdf3e342b5dda2150f502f257c0e8fe07ec2c3
riskisitorus <riskijkm@gmail.com>
2024-09-19 14:08:24 +0700

    update CORS

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializers
-from projects.models import Project, Tag
+from projects.models import Project, Tag, Review
 from users.models import Profile
 
 @api_view(['GET'])
@@ -17,6 +18,7 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializers(projects, many=True)
@@ -25,7 +27,27 @@ def getProjects(request):
 
 @api_view(['GET'])
 def getProject(request,pk):
-    projects = Project.objects.get(id=pk)
-    serializer = ProjectSerializers(projects, many=False)
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializers(project, many=False)
+    context = {}
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request,pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
+    serializer = ProjectSerializers(project, many=False)
     context = {}
     return Response(serializer.data)
